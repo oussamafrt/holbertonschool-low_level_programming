3a0edd6326d9692ad12f9dfcdfe9f508f898fc89
root <ouss.frt12@gmail.com>
2024-02-19 13:17:28 +0000

    project 1 write a program

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main ()
 {
-puts("Programming is like building a multilingual puzzle)
-return o;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
