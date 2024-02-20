a20c34ae713640652877eecb673b02d18338733d
root <ouss.frt12@gmail.com>
2024-02-20 06:30:36 -0800

    project 2 task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'd' ; ch++)
+	{
+	putchar(ch);
+	}
+	for (ch = 'f' ; ch <= 'p' ; ch++)
+	{
+	putchar(ch);
+	}
+	for (ch = 'r' ; ch <= 'z' ; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	return(0);
+}
