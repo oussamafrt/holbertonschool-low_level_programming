d35ef8322f471b3f42c2187a742daf8f7811c197
root <ouss.frt12@gmail.com>
2024-02-20 01:40:11 -0800

    project 2 variable positive of negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{ 
-	printf("%d is positif\n",n);
+	printf("%d is positive\n",n);
 	}
 	if (n < 0) 
 	{
-	printf("%d is negatif\n",n);
+	printf("%d is negative\n",n);
 	}
 	if (n == 0)
 	{
