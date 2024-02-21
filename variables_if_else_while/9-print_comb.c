420a5fb29c8f827706c336fbd64075214a876ba2
root <ouss.frt12@gmail.com>
2024-02-21 01:08:38 -0800

    project 2 digital number

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	{
+		for (ch = '1' ; ch <= '9' ; ch++)
+		{
+		{
+		putchar(ch);
+		}
+		if (ch != '9')
+		{
+		putchar(',');
+		}
+		{
+		putchar(' ');
+		}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
