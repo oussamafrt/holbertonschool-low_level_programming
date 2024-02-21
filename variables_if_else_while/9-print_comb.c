1edcc7b2b57b268f830c321eebfb7ab7f3f8423b
root <ouss.frt12@gmail.com>
2024-02-21 02:08:38 -0800

    project 2 task 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,19 +10,13 @@ int main(void)
 {
 	char ch;
 
+	for (ch = '0' ; ch <= '9' ; ch++)
 	{
-		for (ch = '0' ; ch <= '9' ; ch++)
-		{
-		{
 		putchar(ch);
-		}
 		if (ch != '9')
 		{
-		putchar(',');
-		}
-		{
-		putchar(' ');
-		}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
