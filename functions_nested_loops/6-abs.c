3e39f6a9cb1bf043977ab9cff5026685f407a2fb
root <ouss.frt12@gmail.com>
2024-02-22 15:50:52 +0000

    project 3 task abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * @n: character to be verified
+ *
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
