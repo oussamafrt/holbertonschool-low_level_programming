6e7fada29b8a90a6229093e8ce8fa273e159c2ed
root <ouss.frt12@gmail.com>
2024-02-22 09:39:47 +0000

    project 3 print alph

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,12 +6,12 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void);
-
+void print_alphabet(void)
 {
 	char ch;
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
 		_putchar(ch);
 	}
+	_putchar('\n');
 }
