3f3c4289230bbe0dd1ce6dd7641fc47e5a777f02
root <ouss.frt12@gmail.com>
2024-02-23 16:06:53 +0000

    project 3 time table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+   * times_table - Entry point
+ *
+ * Return: absolute value
+ */
+
+void times_table(void)
+{
+	int a, b;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		for (b = 0 ; b < 10 ; b++)
+		{
+			if (b != 0)
+			{
+				if (a * b < 10)
+				{
+				_putchar (',');
+				_putchar (' ');
+				_putchar (' ');
+				_putchar ('0' + a * b);
+				}
+			if (a * b > 10)
+			{
+				_putchar (',');
+				_putchar (' ');
+				_putchar ('0' + a * b / 10);
+				_putchar ('0' + a * b % 10);
+			}
+			}
+			else
+			{
+				_putchar('0');
+			}
+		}
+	}
+}
