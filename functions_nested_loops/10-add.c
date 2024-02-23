40ac9ed40e55cad7fc4dc442b93fd18b62d362c9
root <ouss.frt12@gmail.com>
2024-02-23 14:16:29 +0000

    project 3 add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * @a: character to be verified
+ * @b: character to be verified
+ *
+ * Return: absolute value
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
