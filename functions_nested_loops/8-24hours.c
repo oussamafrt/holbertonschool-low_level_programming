1ce811fd1ba3ee024fd2f83765bdd27702639275
root <ouss.frt12@gmail.com>
2024-02-23 14:18:10 +0000

    project 3 add

---
+++ functions_nested_loops/8-24hours.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * void jack_bauer - Entry point
+ *
+ * @n: character to be verified
+ *
+ * Return: absolute value
+ */
+
+void jack_bauer(void)
+{
+	int minute, int hour;
+	for (minute < 0 ; minute <= 59 ; minute++);
+
