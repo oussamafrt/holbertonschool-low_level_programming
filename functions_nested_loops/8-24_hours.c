7e0ce67cd35482d103e812d41e8dd448af716708
root <ouss.frt12@gmail.com>
2024-02-23 15:19:42 +0000

    project 3 task hours

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: absolute value
+ */
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0 ; h < 24 ; h++)
+	{
+		for (m = 0 ; m < 60 ; m++)
+		{
+			if (h < 10)
+			{
+				_putchar ('0');
+				_putchar ('0' + h);
+			}
+			else
+			{
+				_putchar ('0' + h / 10);
+				_putchar ('0' + h % 10);
+			}
+			_putchar (':');
+			if (m < 10)
+			{
+				_putchar ('0');
+				_putchar ('0' + m);
+			}
+			else
+			{
+				_putchar ('0' + m / 10);
+				_putchar ('0' + m % 10);
+			}
+			_putchar ('\n');
+		}
+	}
+}
