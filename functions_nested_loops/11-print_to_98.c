e2d29cdf52f5d35e6b5ccef57016b31f55c88fbc
root <oussama.ferhat01@gmail.com>
2024-02-25 21:46:51 +0000

    project 3 last task

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,11 +14,11 @@ void print_to_98(int n)
 	int i;
 
 	{
-	for (i = n ; i < 98 ; i++)
+	for (i = n ; i <= 98 ; i++)
 	{
 		printf("%d, ", i);
 	}
-	for (i = n ; i > 98 ; i--)
+	for (i = n ; i >= 98 ; i--)
 	{
 		printf("%d, ", i);
 	}
