92bb0e44c81d3b06d1b942ff13cc20c5b93b33a5
root <oussama.ferhat01@gmail.com>
2024-02-25 22:05:33 +0000

    project 3 last task

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,32 +2,43 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - Entry point
- *
- * @n: character to be verified
- *
- * Return: absolute value
- */
-
+  * print_to_98 - Entry point
+  * @n: character to be verified
+  *
+  * Return: Absolute value
+  */
 void print_to_98(int n)
 {
-	int i;
-
+	if (n <= 98)
 	{
-	for (i = n ; i <= 98 ; i++)
-	{
-		printf("%d, ", i);
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
 	}
-	for (i = n ; i >= 98 ; i--)
+	else
 	{
-		printf("%d, ", i);
-	}
-	for (; i == 98 ;)
-	{
-		printf("%d", i);
-		break;
-	}
-
-	printf("\n");
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
 	}
 }
