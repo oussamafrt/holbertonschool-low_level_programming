faf9f444bb87f770c0bce0fb1059445e879416e4
root <oussama.ferhat01@gmail.com>
2024-02-26 09:45:17 +0000

    project 1 multiplies integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Entry point
+ * @a : character to be verified
+ * @b : character to be verified
+ * Return: absolute value
+ */
+
+int mul(int a, int b)
+{
+	{
+	putchar (a * b);
+	}
+	return (a * b);
+}
