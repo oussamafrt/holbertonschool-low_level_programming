b488abe2d4e58cbd6ac8f8ef4554b74a3cff23e8
root <oussama.ferhat01@gmail.com>
2024-02-26 21:16:18 +0000

    FizzBuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,23 +7,30 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
+
 {
 	int i;
 
 	{
-		for (i = 0 ; i <= 100 ; i++)
+		for (i = 1 ; i <= 100 ; i++)
 			if (i % 3 == 0 && i % 5 == 0)
 			{
-				printf("FizzBuzz");
+				printf("FizzBuzz ");
 			}
-			else if ( i % 3 == 0)
+			else if (i % 3 == 0)
 			{
-				printf("Fizz");
+				printf("Fizz ");
 			}
 			else if (i % 5 == 0)
 			{
-				printf("Buzz");
+				printf("Buzz ");
 			}
+			else
+			{
+				printf("%d ", i);
+			}
+		printf("\n");
 	}
+	return (0);
 }
