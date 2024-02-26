2333879397fd7e6d3f8a5fef4ad0d6164203ca36
root <oussama.ferhat01@gmail.com>
2024-02-26 11:01:04 +0000

    project 3 print num

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Return: absolute value
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0 ; a <= 9 ; a++)
+	{
+	if (a == 2)
+	{
+		continue;
+	}
+	if (a == 4)
+	{
+		continue;
+	}
+	else
+	{
+		_putchar ('0' + a);
+	}
+	}
+	_putchar ('\n');
+}
