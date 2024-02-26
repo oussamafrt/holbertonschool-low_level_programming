7e05c1810af3710fbcf621320fd4f4933fd69d7f
root <oussama.ferhat01@gmail.com>
2024-02-26 15:47:34 +0000

    print square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - Entry point
+ *@n : character to be verified
+ *
+ * Return: Always 0.
+ */
+
+void print_square(int size)
+{
+	int i, u;
+
+	if (size > 0)
+	{
+		for (i = 0 ; i < size ; i++)
+		{
+			for (u = 1 ; u  <= size ; u++)
+			{
+				_putchar ('#');
+			}
+				_putchar ('\n');
+		}
+	}
+	else
+	{
+		_putchar ('\n');
+	}
+}
+
