ce487d50776d0351d3ec6eaccafde38b9ef752a8
root <oussama.ferhat01@gmail.com>
2024-02-26 13:40:36 +0000

    project 1 print number

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ *
+ * Return: absolute value
+ */
+
+void more_numbers(void)
+{
+	int i, a;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (a = 0 ; a <= 14 ; a++)
+			if (a <= 9)
+			{
+				_putchar ('0' + a);
+			}
+			else
+			{
+				_putchar ('0' + a / 10);
+				_putchar ('0' + a % 10);
+			}
+		{
+			_putchar ('\n');
+		}
+	}
+}
