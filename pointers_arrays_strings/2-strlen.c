63d6cc89a920a919d89e1f27d82d4ee3a38db6be
root <oussama.ferhat01@gmail.com>
2024-02-27 14:28:15 +0000

    sterlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * @s: character to print
+ * @a: character to be print
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	a = '\0';
+		while (s[a] != '\0')
+		{
+			a++;
+		}
+		return (a);
+}
