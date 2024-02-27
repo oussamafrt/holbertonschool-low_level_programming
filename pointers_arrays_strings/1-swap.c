783e470b5aee5a153251e35b97a34eb6d0ed0d14
root <oussama.ferhat01@gmail.com>
2024-02-27 13:21:23 +0000

    swap value

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap - Entry point
+ * @a: character to be verified
+ * @b: character to be verified
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
+
