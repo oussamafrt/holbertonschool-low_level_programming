ef709d7cb549fa9507d49eba2d180e652b57373e
root <oussama.ferhat01@gmail.com>
2024-02-27 09:01:25 +0000

    print triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,17 +9,20 @@
 
 void print_triangle(int size)
 {
-	int i, u;
+	int i, j, k;
 
 	if (size > 0)
 	{
-		for (i = 0 ; i < size ; i++)
+		for (i = 1 ; i <= size ; i++)
 		{
-			for (u = 1 ; u  > i ; u--)
+			for (j = 1 ; j <= size - i ; j++)
 			{
 				_putchar (' ');
 			}
+			for (k = 1 ; k <= i ; k++)
+			{
 			_putchar ('#');
+			}
 			_putchar ('\n');
 		}
 	}
