10166200a681d1877975c64fd246873e76fab5ec
oussamafrt <oussama.ferhat01@gmail.com>
2024-02-29 13:41:45 +0100

    print half script

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - Entry point
+ *
+ * @str: character to be print
+ *
+ * Return: Always 0
+ */
+
+void puts_half(char *str)
+{
+	int l, n, i;
+
+	l = 0;
+
+	while (str[l] != 0)
+	{
+		l++;
+	}
+	if (l % 2 == 1)
+	{
+		n = (l - 1) / 2;
+	}
+	n = l / 2;
+
+	for (i = n ; i < l ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
