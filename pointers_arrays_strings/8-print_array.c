2b8e7321666ec283c8aa19697bc7f68163048176
oussamafrt <oussama.ferhat01@gmail.com>
2024-03-01 11:09:16 +0100

    print array element

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array : Entry point
+ *
+ * n@: character to print
+ *
+ * Return: Always 0;
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		if (i < 4)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
+	}
+	printf("\n");
+}	
