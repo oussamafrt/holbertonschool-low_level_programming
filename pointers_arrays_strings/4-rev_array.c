62a6ce959feeee89fb6f1d7459295d2446db7be9
oussamafrt <oussama.ferhat01@gmail.com>
2024-03-03 21:00:46 +0100

    reverse content

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - Entry point
+ * @a: array of integers
+ * @n: number of elements in array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int h;
+
+	for (i = 0; i < (n / 2); i++)
+	{
+		h = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = h;
+	}
+}
