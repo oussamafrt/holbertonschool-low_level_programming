d6d47a22e4c8cea6d6bc121643f4f1ada299d3d1
oussamafrt <oussama.ferhat01@gmail.com>
2024-03-03 19:20:11 +0100

    concatenates two strings

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncat - Entry point
+ * @dest: character to be print
+ * @src: character to be print
+ * Return: Always 0
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, c;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
+	{
+		dest[i] = src[c];
+	}
+	return (dest);
+}
