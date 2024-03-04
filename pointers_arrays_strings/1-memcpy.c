1d74d49ccd72e51532e0a8d26e31f3fa6ee7b748
oussamafrt <oussama.ferhat01@gmail.com>
2024-03-04 10:57:41 +0100

    memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - Entry point
+ * @dest: character to print
+ * @src: character to print
+ * @n: character to print
+ *
+ * Return: Nothing.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		dest[i] = src [i];
+	}
+	return (dest);
+}
