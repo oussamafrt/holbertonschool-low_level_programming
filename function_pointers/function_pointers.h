6133d17e3b8efa1102e96880dea4c77c39e33979
oussamafrt <oussama.ferhat01@gmail.com>
2024-03-18 11:57:53 +0100

    new directory

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,9 +1,9 @@
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
-
-#ifndef FUNCTION_POINTER_H
-#define FUNCTION_POINTER_H
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
