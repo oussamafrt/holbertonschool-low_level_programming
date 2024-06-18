03c7dcd936f495257ecd6e85b7209d35ab9f7ea4
oussamafrt <oussama.ferhat01@gmail.com>
2024-06-18 12:40:36 +0200

    Write a function that adds two integers and returns the result.

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @a: character to be verified
+ * @b: character to be verified
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
