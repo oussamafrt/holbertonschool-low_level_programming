e926a29916681854d017f519e3173c92d116ad59
oussamafrt <oussama.ferhat01@gmail.com>
2024-08-03 13:44:42 +0200

    C - Singly linked lists

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,13 +1,25 @@
 #ifndef LINKED_LISTS
 #define LINKED_LISTS
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
 } list_t;
-E size_t print_list(const list_t *h);
-E size_t list_len(const list_t *h);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
