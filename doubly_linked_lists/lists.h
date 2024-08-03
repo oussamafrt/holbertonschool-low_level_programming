02dbe355385ab87847d4269a21acfe9be1f0c884
oussamafrt <oussama.ferhat01@gmail.com>
2024-08-03 15:03:23 +0200

    Write a function that adds a new node at the end of a dlistint_t list.

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,5 +1,6 @@
-#ifndef DOUBLY_LINKED_LISTS
-#define DOUBLY_LINKED_LISTS
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -10,7 +11,7 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
+ *
  */
 typedef struct dlistint_s
 {
@@ -18,6 +19,7 @@ typedef struct dlistint_s
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
 } dlistint_t;
+
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
@@ -27,4 +29,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
 #endif
